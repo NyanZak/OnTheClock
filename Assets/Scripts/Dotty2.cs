e60233a4790a546e188ac273c570d2118e40206c
Zak Cronin <71699711+NyanZak@users.noreply.github.com>
2023-07-06 01:03:01 +0100

    final

---
+++ Assets/Scripts/Dotty2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dotty2 : MonoBehaviour
+{
+    public Transform centerImage;
+    public float rotationSpeed = 5f;
+
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 direction = mousePosition - centerImage.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+}
